cb4df78ba4bb75e32a23affe3744f2f73cf655d8
danajwright <danajwright@gmail.com>
2014-02-10 13:50:05 -0800

    edits to headline

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,21 +1,5 @@
 
 
-//GOOGLE ANALYTICS
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47927538-1', 'bitspace360.com');
-  ga('send', 'pageview');
-
-</script>
-
-
-
-
 //SCROLL TO ANCHOR
 
 function scrollToAnchor(aid){
